9232b4034a45fd4b00610c702f6eaf6036f3a76f
Vlasis Mentz <vlasism@gmail.com>
2022-01-03 12:09:51 +0200

    imnplementation of spinner

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import NavbarComponent from './components/NavbarComponent/NavbarComponent';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import GamesContainer from './components/GamesContainer/GamesContainer';
 import LogoComponent from './components/LogoComponent/LogoComponent';
+import { Spinner } from 'react-bootstrap';
 
 function App() {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   async function getGames() {
     await axios
       .get('https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=50')
       .then((response) => {
         setData(response.data);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.log(error);
@@ -23,9 +25,17 @@ function App() {
     getGames();
   }, []);
 
-  return (
+  return isLoading ? (
     <>
-      <NavbarComponent />
+      <LogoComponent />
+      <div className='spinner'>
+        <div className='spinner-animation'>
+          <Spinner animation='border' variant='warning' />
+        </div>
+      </div>
+    </>
+  ) : (
+    <>
       <LogoComponent />
       <GamesContainer data={data} />
     </>
--- src/components/LogoComponent/LogoComponent.jsx
+++ src/components/LogoComponent/LogoComponent.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import LogoSteam from './../../assets/steam-logo-black-transparent.png';
 import './LogoComponent.styles.css';
+import { FaSteam } from 'react-icons/fa';
 const LogoComponent = () => {
   return (
     <div className='steam-deals'>
       <h1 className='second-title'>
-        Steam <img className='logo-image' src={LogoSteam}></img> Deals
+        Steam <FaSteam /> Deals
       </h1>
     </div>
   );
